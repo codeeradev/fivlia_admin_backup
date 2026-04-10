89ec19e3e25570206a434bfc82d4c090a99215bc
Your Name <you@example.com>
2026-04-10 11:30:42 +0530

    update

--- src/api/endPoints.js
+++ src/api/endPoints.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "https://api.fivlia.com";
-// export const API_BASE_URL = "https://api.fivlia.in";
+// export const API_BASE_URL = "https://api.fivlia.com";
+export const API_BASE_URL = "https://api.fivlia.in";
 // export const API_BASE_URL = "http://localhost:8080";
 // export const API_BASE_URL = "https://api.fivlia.co.in";
 
